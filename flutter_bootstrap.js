25c08c1d639313e863e4ac1aa6dfcb8f993c9741
Shanu <Shahnawaz.sk230@gmail.com>
2025-08-30 16:13:14 +0530

    initial commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "190693771"
+    serviceWorkerVersion: "306809028"
   }
 });
